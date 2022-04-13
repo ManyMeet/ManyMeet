ce23d7cce2548f05751a71112c1f0383ea051cda
RR <roznoshchik@gmail.com>
2022-04-13 11:36:45 +0200

    feat: added router to app and added necessary http modules for api service

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GoogleAuthComponent } from './components/google-auth/google-auth.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent}
+  {path:'', component: HomeComponent},
+  {path:'auth/google', component: GoogleAuthComponent}
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,26 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import {adapterFactory} from 'angular-calendar/date-adapters/date-fns'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { GoogleAuthComponent } from './components/google-auth/google-auth.component';
+import { MonitorInterceptor } from './monitor.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    GoogleAuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     BrowserAnimationsModule,
+    HttpClientModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory,
     })
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS, useClass:MonitorInterceptor, multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
