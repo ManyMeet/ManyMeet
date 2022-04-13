3a3386330fdd0f54d7a25edee18a2ec54856e5b9
RR <roznoshchik@gmail.com>
2022-04-13 11:32:48 +0200

    feat: have a basic calendar page component using angular-calendar

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,34 +1,98 @@
-import { Component, OnInit, ɵɵsetComponentScope } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CalendarView, CalendarEvent } from 'angular-calendar';
-import { startOfDay } from 'date-fns';
+import { CalendarService } from 'src/app/services/calendar.service';
+
+const now = new Date();
+const dog = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0,0);
+const dogEnd = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0,0);
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: [ '../../../../node_modules/angular-calendar/css/angular-calendar.css', './home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  viewDate: Date = new Date();
-  view: CalendarView = CalendarView.Month;
-  CalendarView = CalendarView;
   
+  viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Week;
+  CalendarView = CalendarView;
+  authorized: string | null = localStorage.getItem('authorized') ;
   events: CalendarEvent[] = [
     {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date'
+      start: dog,
+      end: dogEnd,
+      title: 'A doggo event',
+      color: {primary: '#ad2121', secondary: "#fff"}
     }
   ]
+  allowance: number = 10;
+  created: number = 0;
+  constructor (private api: CalendarService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit () : void {
+  } 
 
   changeView(view: CalendarView) {
     this.view = view;
   }
 
-  dayClicked({date, events}: {date:Date; events: CalendarEvent[]}) :void {
-    console.log(date)
+  dayClicked(date:Date, events?:CalendarEvent[]) :void {
+    // console.log(date)
+    if (this.created < this.allowance) {
+      const newEvent: CalendarEvent = {
+        start:date,
+        end: new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes()+45),
+        title: 'New Event'
+      }
+      this.events = [
+        ...this.events,
+        newEvent
+      ]
+      this.created++;
+    }
+
+    // console.log(events)
   }
+
+  clickedDate: Date | undefined = undefined;
+  clickedColumn: number | undefined= undefined;
+
+  google (): void {
+    this.api.authorize()
+  }
+
+  // async getEvents ():  {
+  //   const headers = new Headers();
+  //   headers.append('Authorization', 'bearer ' + this.access_token)
+  //   return new Promise()
+  // }
+
+    getEvents() : void {
+      const now = new Date();
+      const startDate = new Date(now.getFullYear(), now.getMonth(), 1).toISOString()
+      const lastDate = new Date(now.getFullYear(), now.getMonth() + 1, 0).toISOString()
+
+      const searchParams : {[key:string]: string} = {
+        timeMin:startDate,
+        timeMax:lastDate,
+        orderBy: 'startTime',
+        singleEvents:'True'
+      }
+      this.api.getEvents(searchParams).subscribe(events => {
+        for (let ev of events) {
+          this.events = [
+            ...this.events,
+            {
+              start: new Date(ev.start.dateTime) || new Date(ev.start.date), 
+              end: new Date(ev.end.dateTime) || new Date(ev.end.date),
+
+              title:ev.summary
+            }
+          ]
+        }
+
+
+      });
+    }
+
 }
